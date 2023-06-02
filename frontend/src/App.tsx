704dfd1047607010a8559060f4345b1967122026
noobdy <htommyf12@gmail.com>
2023-06-03 04:39:50 +0800

    added dark theme and inter font

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,20 +5,28 @@ import Dead from './Dead';
 
 const App: Component = () => {
     return (
-        <div class="flex flex-col items-center">
-            <div class="my-10 flex space-x-5">
-                <A href="/" class="text-4xl">
-                    Hello
-                </A>
-                <A href="/dead" class="text-4xl">
-                    Dead
-                </A>
-            </div>
+        <div class="h-screen bg-neutral-900 font-bold text-neutral-200">
+            <div class="flex flex-col items-center">
+                <div class="my-10 flex space-x-5">
+                    <A
+                        href="/"
+                        class="text-4xl transition hover:text-neutral-400"
+                    >
+                        Hello
+                    </A>
+                    <A
+                        href="/dead"
+                        class="text-4xl transition hover:text-neutral-400"
+                    >
+                        Dead
+                    </A>
+                </div>
 
-            <Routes>
-                <Route path="/" component={Hello} />
-                <Route path="/dead" component={Dead} />
-            </Routes>
+                <Routes>
+                    <Route path="/" component={Hello} />
+                    <Route path="/dead" component={Dead} />
+                </Routes>
+            </div>
         </div>
     );
 };
